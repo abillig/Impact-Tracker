f8d69e1dcdbac93067365691a73b8328bc919dd2
abillig <avrambillig@gmail.com>
2017-02-07 16:39:27 -0500

    remove impact types show page and action

--- app/controllers/impact_types_controller.rb
+++ app/controllers/impact_types_controller.rb
@@ -4,16 +4,4 @@ class ImpactTypesController < ApplicationController
     @impact_types = ImpactType.all
   end
 
-  def show
-    @impact_type = ImpactType.find(params[:id])
-    @impact_types = ImpactType.all
-    #for development
-      # @impacts = Impact.all[1..10]
-    #for production
-    @impacts = Impact.impacts_from(current_user.publication).select{|impact|impact.impact_type == @impact_type}
-    @num_impacts = @impacts.length
-
-  end
-
-
 end
